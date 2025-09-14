c32bcd1c9d7a121793e11652da0d35bd2c0b17ab
Captain Sero <yasoo976098@gmail.com>
2025-09-14 20:24:59 +0300

    add stuff container

--- lib/features/dash_board/screens/days_data/presentation/screen/days_data.dart
+++ lib/features/dash_board/screens/days_data/presentation/screen/days_data.dart
@@ -7,6 +7,7 @@ import 'package:team_egypt_v3/features/dash_board/screens/days_data/logic/days_d
 import 'package:team_egypt_v3/features/dash_board/screens/days_data/presentation/widget/customers_table.dart';
 import 'package:team_egypt_v3/features/dash_board/screens/days_data/presentation/widget/date_picker_button.dart';
 import 'package:team_egypt_v3/features/dash_board/screens/days_data/presentation/widget/rooms_reservation_card.dart';
+import 'package:team_egypt_v3/features/dash_board/screens/days_data/presentation/widget/stuff_data.dart';
 import 'package:team_egypt_v3/features/dash_board/screens/days_data/presentation/widget/total_salary_card.dart';
 import 'package:team_egypt_v3/features/time_screen/data/supabase_in_team.dart';
 
@@ -80,6 +81,10 @@ class _DaysDataState extends State<DaysData> {
               const SizedBox(height: 10),
 
               RoomsReservationCard(dateFormat: dateFormat),
+
+              const SizedBox(height: 10),
+
+              StuffData(dateFormat: dateFormat),
             ],
           ),
         );
---
+++ lib/features/dash_board/screens/days_data/presentation/widget/stuff_data.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:team_egypt_v3/core/constants/color.dart';
+import 'package:team_egypt_v3/core/constants/screen_size.dart';
+import 'package:team_egypt_v3/core/models/reservation_model.dart';
+import 'package:team_egypt_v3/core/utils/string_extensions.dart';
+import 'package:team_egypt_v3/features/dash_board/screens/days_data/logic/days_data_cubit/days_data_cubit.dart';
+import 'package:team_egypt_v3/features/dash_board/widgets/table_cell.dart';
+import 'package:team_egypt_v3/features/dash_board/widgets/table_header.dart';
+
+class StuffData extends StatelessWidget {
+  StuffData({super.key, required this.dateFormat});
+
+  final String dateFormat;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenSize.width / 1.5,
+      height: ScreenSize.height / 3,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Col.dark2,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Stuff Data - $dateFormat",
+                  style: TextStyle(
+                    color: Col.light2,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              BlocBuilder<DaysDataCubit, DaysDataState>(
+                builder: (context, state) {
+                  List<ReservationModel> reservation = [];
+                  if (state is DayCustomersLoad) {
+                    reservation = state.reservations;
+                  }
+                  return Table(
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                    columnWidths: const {
+                      0: FlexColumnWidth(2),
+                      1: FlexColumnWidth(2),
+                      2: FlexColumnWidth(2),
+                    },
+                    children: [
+                      TableRow(
+                        children: [
+                          TableHeader("Name"),
+                          TableHeader("Number"),
+                          TableHeader("Position"),
+                          Center(child: TableHeader("Checkin-Checkout")),
+                        ],
+                      ),
+                      for (var ele in reservation)
+                        TableRow(
+                          children: [
+                            TableCell1(ele.name),
+                            TableCell1(ele.number),
+                            TableCell1(ele.room),
+                            TableCell1(StringExtensions.formatDate(ele.date)),
+                          ],
+                        ),
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
